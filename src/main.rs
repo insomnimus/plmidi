e51aaa4bac342979698075e9e3a2fe9f95fb9d3a
Taylan Gökkaya <insomnimus.dev@gmail.com>
2021-10-04 23:51:45 +0300

    feat: added pause/unpause controls

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,46 @@
 mod app;
 
-use std::{convert::TryFrom, error::Error, fs, process, time::Duration};
+use std::{
+	convert::TryFrom,
+	fs,
+	io::{self, Write},
+	path::PathBuf,
+	process,
+	sync::mpsc::{self, SyncSender},
+	thread,
+	time::Duration,
+};
 
+use anyhow::{bail, ensure, Error};
+use crossterm::{
+	event::{self, Event, KeyCode, KeyModifiers},
+	terminal::{disable_raw_mode, enable_raw_mode, is_raw_mode_enabled, Clear, ClearType},
+	ExecutableCommand,
+};
 use log::Level;
 use midir::{MidiOutput, MidiOutputConnection};
 use midly::{Format, Smf};
 use nodi::{Player, Sheet, Ticker, Timer};
+
+fn print(s: &str) {
+	fn inner(s: &str) -> Result<(), io::Error> {
+		let mut stdout = io::stdout();
+		stdout.execute(Clear(ClearType::UntilNewLine))?;
+		for (i, ln) in s.lines().filter(|s| !s.is_empty()).enumerate() {
+			if i > 0 {
+				writeln!(&mut stdout)?;
+			}
+			write!(&mut stdout, "{}\r", ln)?;
+			stdout.flush()?;
+		}
+		Ok(())
+	}
+	if let Ok(true) = is_raw_mode_enabled() {
+		let _ = inner(s);
+	} else {
+		println!("{}", s);
+	}
+}
 
 fn format_duration(t: Duration) -> String {
 	let secs = t.as_secs();
@@ -18,7 +53,7 @@ fn format_duration(t: Duration) -> String {
 	}
 }
 
-fn list_devices() -> Result<(), Box<dyn Error>> {
+fn list_devices() -> Result<(), Error> {
 	let midi_out = MidiOutput::new("nodi")?;
 
 	let out_ports = midi_out.ports();
@@ -41,27 +76,25 @@ fn list_devices() -> Result<(), Box<dyn Error>> {
 	Ok(())
 }
 
-fn get_midi(n: usize) -> Result<MidiOutputConnection, Box<dyn Error>> {
+fn get_midi(n: usize) -> Result<MidiOutputConnection, Error> {
 	let midi_out = MidiOutput::new("nodi")?;
 
 	let out_ports = midi_out.ports();
 	if out_ports.is_empty() {
-		return Err("no MIDI output device detected".into());
+		bail!("no MIDI output device detected");
 	}
-	if n >= out_ports.len() {
-		return Err(format!(
-			"only {} MIDI devices detected; run with --list  to see them",
-			out_ports.len()
-		)
-		.into());
-	}
+	ensure!(
+		n < out_ports.len(),
+		"only {} MIDI devices detected; run with --list  to see them",
+		out_ports.len()
+	);
 
 	let out_port = &out_ports[n];
 	let out = midi_out.connect(out_port, "cello-tabs")?;
 	Ok(out)
 }
 
-fn run() -> Result<(), Box<dyn Error>> {
+fn run() -> Result<(), Error> {
 	let m = app::new().get_matches();
 	if m.is_present("list") {
 		return list_devices();
@@ -79,12 +112,13 @@ fn run() -> Result<(), Box<dyn Error>> {
 		.map(|s| s.parse::<i8>().unwrap())
 		.unwrap_or(0);
 	let n_device = m.value_of("device").unwrap().parse::<usize>().unwrap();
-	let file_name = m.value_of("file").unwrap();
+	let file_name = PathBuf::from(m.value_of("file").unwrap());
 
 	let out = get_midi(n_device)?;
-	let data = fs::read(file_name)?;
+	let data = fs::read(&file_name)?;
 
 	let Smf { header, tracks } = Smf::parse(&data)?;
+	let (sender, receiver) = mpsc::sync_channel(1);
 
 	let mut timer = Ticker::try_from(header.timing)?;
 	timer.speed = speed;
@@ -96,21 +130,63 @@ fn run() -> Result<(), Box<dyn Error>> {
 
 	sheet.transpose(transpose, false);
 
-	println!("Playing {}", &file_name);
+	println!(
+		"Playing {}",
+		&file_name.file_name().unwrap_or_default().to_string_lossy()
+	);
 	let mut t = timer;
 	println!(
 		"Total duration: {}",
 		format_duration(t.duration(&sheet[..]))
 	);
 
+	let timer = timer.to_control(receiver);
+	thread::spawn(move || {
+		listen_keys(sender);
+	});
+
 	let mut player = Player::new(timer, out);
 	player.play_sheet(&sheet);
 	Ok(())
 }
 
+fn listen_keys(sender: SyncSender<()>) {
+	if let Err(e) = enable_raw_mode() {
+		eprintln!("warning: failed to enable raw input mode: {}", e);
+	}
+	print("press the spacebar to play/pause, esc to quit");
+	let mut paused = false;
+	loop {
+		let k = match event::read() {
+			Ok(Event::Key(k)) => k,
+			_ => continue,
+		};
+		match k.code {
+			KeyCode::Esc => break,
+			KeyCode::Char('c') if k.modifiers == KeyModifiers::CONTROL => break,
+			KeyCode::Char(' ') => {
+				sender.send(()).unwrap();
+				if paused {
+					print("unpaused");
+				} else {
+					print("paused");
+				}
+				paused = !paused;
+			}
+			_ => (),
+		};
+	}
+	process::exit(if let Err(e) = disable_raw_mode() {
+		eprintln!("warning: failed to disable raw input mode: {}", e);
+		1
+	} else {
+		0
+	});
+}
+
 fn main() {
 	if let Err(e) = run() {
-		eprintln!("error: {}", e);
+		eprintln!("error: {:?}", e);
 		process::exit(2);
 	}
 }
